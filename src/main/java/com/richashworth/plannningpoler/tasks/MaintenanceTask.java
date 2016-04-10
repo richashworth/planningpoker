870683e8b3e96dfd37ccbdb463a6eb2843952389
Richard Ashworth <rich@Richards-MacBook-Pro.local>
2016-04-10 19:22:51 +0100

    added autowired annotation

--- src/main/java/com/richashworth/plannningpoler/tasks/MaintenanceTask.java
+++ src/main/java/com/richashworth/plannningpoler/tasks/MaintenanceTask.java
@@ -1,6 +1,7 @@
 package com.richashworth.plannningpoler.tasks;
 
 import com.richashworth.planningpoker.model.SessionManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 public class MaintenanceTask {
     private SessionManager sessionManager;
 
+    @Autowired
     public MaintenanceTask(SessionManager sessionManager) {
         this.sessionManager = sessionManager;
     }
