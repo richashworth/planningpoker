140f9c0018cc3107f9517393e3769b1f956d0e36
Richard Ashworth <rich@Richards-MacBook-Pro.local>
2016-04-17 22:59:25 +0100

    formatted javascript

--- src/main/resources/static/javascript/app.js
+++ src/main/resources/static/javascript/app.js
@@ -2,7 +2,7 @@ var PlanningPoker = angular.module('PlanningPoker', ['chart.js', 'emguo.poller']
 
 PlanningPoker.config(['$httpProvider', function ($httpProvider) {
     if (!$httpProvider.defaults.headers.get) {
-        $httpProvider.defaults.headers.get = {};
+        $httpProvider.defaults.headers.get = {}
     }
 
     //prevent IE caching AJAX requests
@@ -31,11 +31,11 @@ PlanningPoker.controller('PokerCtrl', ['$scope', '$http', 'poller', function ($s
                 userName: $scope.userName
             }
         }).then(function successCallback(response) {
-            $scope.inSession = true;
+            $scope.inSession = true
         }, function errorCallback(response) {
             alert("Session " + $scope.sessionId + " has not yet been started!\n" +
-                "Please try again in a few seconds, or create a new session.");
-        });
+                "Please try again in a few seconds, or create a new session.")
+        })
     };
 
     $scope.createSession = function () {
@@ -49,7 +49,7 @@ PlanningPoker.controller('PokerCtrl', ['$scope', '$http', 'poller', function ($s
             $scope.inSession = true;
             $scope.sessionId = response;
             $scope.isAdmin = true;
-        });
+        })
     };
 
     $scope.vote = function (estimateValue) {
@@ -78,32 +78,27 @@ PlanningPoker.controller('PokerCtrl', ['$scope', '$http', 'poller', function ($s
                 myPoller.promise.then(
                     null, null,
                     function (result) {
-
                         if (result.data.length == 0) {
-                            $scope.voted = false;
+                            $scope.voted = false
                         }
-                        ;
-
                         $scope.votingResults = result.data.sort(function (a, b) {
-                            return a.estimateValue > b.estimateValue;
+                            return a.estimateValue > b.estimateValue
                         });
-
                         $scope.transformed = $scope.votingResults.map(function (val) {
-                            return val.estimateValue;
+                            return val.estimateValue
                         });
-
                         $scope.resultsdata = $scope.legalEstimates.map(function (x) {
                             return $scope.transformed.filter(function (y) {
-                                return x == y;
-                            }).length;
-                        });
+                                return x == y
+                            }).length
+                        })
                     }
-                );
+                )
             },
             function errorCallback(response) {
                 alert("Session " + sessionId + " is not currently active. Please refresh your page.")
             }
-        );
+        )
     };
 
     $scope.reset = function () {
@@ -115,8 +110,8 @@ PlanningPoker.controller('PokerCtrl', ['$scope', '$http', 'poller', function ($s
                 userName: $scope.userName
             }
         }).success(function (response) {
-            $scope.voted = false;
-        });
-    };
+            $scope.voted = false
+        })
+    }
 
 }]);
