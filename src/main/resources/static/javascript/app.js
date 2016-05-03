bd1653844d82a1591e64b30b198ff5216c403352
Richard Ashworth <rich@Richards-MacBook-Pro.local>
2016-05-03 23:32:33 +0100

    users initials in upper case

--- src/main/resources/static/javascript/app.js
+++ src/main/resources/static/javascript/app.js
@@ -17,7 +17,7 @@ PlanningPoker.filter('titlecase', function () {
                 return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
             });
         } else {
-            return input;
+            return input.toUpperCase();
         }
     }
 });
