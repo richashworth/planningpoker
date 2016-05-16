fb620b22596e63785436adda6c4970c5a854bca6
Richard Ashworth <rich@Richards-MacBook-Pro.local>
2016-05-16 23:09:40 +0100

    do not reset results if not voted

--- src/main/resources/static/javascript/app.js
+++ src/main/resources/static/javascript/app.js
@@ -122,7 +122,7 @@ PlanningPoker.controller('PokerCtrl', ['$scope', '$http', function ($scope, $htt
                 stompClient.subscribe("/topic/results/" + $scope.sessionId, function (data) {
                     $scope.$apply(function () {
                         var message = JSON.parse(data.body);
-                        if ($scope.voted && message.length == 0) {
+                        if (message.length == 0 && $scope.votedUsers.contains($scope.userName.toUpperCase())) {
                             $scope.voted = false;
                         } else {
                             $scope.resultsdata = $scope.aggregateResults(message);
