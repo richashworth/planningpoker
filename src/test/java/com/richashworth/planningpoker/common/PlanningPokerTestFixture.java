a299002c3777fff9ca45683ea13ded0545b037f6
Richard Ashworth <rich@Richards-MacBook-Pro.local>
2016-08-14 20:02:26 +0100

    added test cases for MessagingUtils

--- src/test/java/com/richashworth/planningpoker/common/PlanningPokerTestFixture.java
+++ src/test/java/com/richashworth/planningpoker/common/PlanningPokerTestFixture.java
@@ -10,8 +10,10 @@ import java.util.List;
  */
 public class PlanningPokerTestFixture {
     public static final Long SESSION_ID = 1L;
+    public static final String ITEM = "A User Story";
     public static final String USER_NAME = "Rich";
     public static final Double ESTIMATE_VALUE = 2D;
     public static final Estimate ESTIMATE = new Estimate(USER_NAME, ESTIMATE_VALUE);
     public static final List<Estimate> RESULTS = Lists.newArrayList(ESTIMATE);
+    public static final List<String> USERS = Lists.newArrayList(USER_NAME);
 }
---
+++ src/test/java/com/richashworth/planningpoker/util/MessagingUtilsTest.java
@@ -0,0 +1,73 @@
+package com.richashworth.planningpoker.util;
+
+import com.richashworth.planningpoker.service.SessionManager;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static com.richashworth.planningpoker.common.PlanningPokerTestFixture.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Rich Ashworth on 14/08/2016.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class MessagingUtilsTest {
+
+    @InjectMocks
+    private MessagingUtils messagingUtils;
+
+    @Mock
+    private SessionManager sessionManager;
+
+    @Mock
+    private SimpMessagingTemplate template;
+
+    @Before
+    public void setUp() {
+        ReflectionTestUtils.setField(messagingUtils, "template", template);
+    }
+
+    @Test
+    public void sendResultsMessage() throws Exception {
+        when(sessionManager.getResults(SESSION_ID)).thenReturn(RESULTS);
+        messagingUtils.sendResultsMessage(SESSION_ID);
+        verify(template).convertAndSend("/topic/results/" + SESSION_ID, RESULTS);
+    }
+
+    @Test
+    public void sendUsersMessage() throws Exception {
+        when(sessionManager.getSessionUsers(SESSION_ID)).thenReturn(USERS);
+        messagingUtils.sendUsersMessage(SESSION_ID);
+        verify(template).convertAndSend("/topic/users/" + SESSION_ID, USERS);
+    }
+
+    @Test
+    public void sendItemMessage() throws Exception {
+        when(sessionManager.getCurrentItem(SESSION_ID)).thenReturn(ITEM);
+        messagingUtils.sendItemMessage(SESSION_ID);
+        verify(template).convertAndSend("/topic/item/" + SESSION_ID, ITEM);
+    }
+
+    @Test
+    public void burstResultsMessages() throws Exception {
+
+    }
+
+    @Test
+    public void burstUsersMessages() throws Exception {
+
+    }
+
+    @Test
+    public void burstItemMessages() throws Exception {
+
+    }
+
+}
