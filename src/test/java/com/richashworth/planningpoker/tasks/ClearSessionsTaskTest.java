66967dab243870d4aa1e6824a03eca3b66be3545
Richard Ashworth <rich@Richards-MacBook-Pro.local>
2016-08-14 21:19:50 +0100

    added test case for ClearSessionsTask

---
+++ src/test/java/com/richashworth/planningpoker/tasks/ClearSessionsTaskTest.java
@@ -0,0 +1,31 @@
+package com.richashworth.planningpoker.tasks;
+
+import com.richashworth.planningpoker.service.SessionManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by Rich Ashworth on 14/08/2016.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ClearSessionsTaskTest {
+
+    @InjectMocks
+    private ClearSessionsTask clearSessionsTask;
+
+    @Spy
+    private SessionManager sessionManager;
+
+    @Test
+    public void testResetSessions() throws Exception {
+        clearSessionsTask.resetSessions();
+        verify(sessionManager, times(1)).clearSessions();
+    }
+
+}
