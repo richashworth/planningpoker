86b4d916ac17cf880f4fafd02103769f572e4e65
Richard Ashworth <richardashworth575@gmail.com>
2016-08-23 22:37:39 +0100

    Update mob_session.sh

--- scripts/mob_session.sh
+++ scripts/mob_session.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for userCount in {1..8}
+for userCount in {1..50}
 do
     curl --data "sessionId=${1}&userName=User ${userCount}" http://localhost:8080/joinSession;
-    curl --data "sessionId=${1}&userName=User ${userCount}&estimateValue=13" http://localhost:8080/vote;
+    curl --data "sessionId=${1}&userName=User ${userCount}&estimateValue=1" http://localhost:8080/vote;
 done
