dfbab030ff6d1c0b70067aa70f4ee8e345ba2ccf
Richard Ashworth <richardashworth575@gmail.com>
2018-08-01 15:39:49 +0100

    join game reducer

--- ui/src/reducers/reducer_game.js
+++ ui/src/reducers/reducer_game.js
@@ -1,13 +1,13 @@
-import { CREATE_GAME } from '../actions'
+import { CREATE_GAME, JOIN_GAME } from '../actions'
 
 const initialGameState = {game: {playerName: '', sessionId: ''}}
 
 export default function(state = initialGameState, action) {
   switch(action.type) {
     case CREATE_GAME:
-    console.log("in reducer");
-      const newState = {playerName: action.meta.userName, sessionId: action.payload.data};
-      return newState
+        return {playerName: action.meta.userName, sessionId: action.payload.data};
+    case JOIN_GAME:
+        return {playerName: action.meta.userName, sessionId: action.meta.sessionId};
     default:
       return state
   }
