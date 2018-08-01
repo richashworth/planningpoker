ae0e5f999e11e32fd807bfec717409f53370be04
Richard Ashworth <richardashworth575@gmail.com>
2018-08-01 15:47:24 +0100

    refactor reducer

--- ui/src/reducers/reducer_game.js
+++ ui/src/reducers/reducer_game.js
@@ -1,13 +1,14 @@
 import { CREATE_GAME, JOIN_GAME } from '../actions'
 
 const initialGameState = {game: {playerName: '', sessionId: ''}}
+const newState = (playerName, sessionId) => ({playerName, sessionId});
 
 export default function(state = initialGameState, action) {
   switch(action.type) {
     case CREATE_GAME:
-        return {playerName: action.meta.userName, sessionId: action.payload.data};
+        return newState(action.meta.userName, action.payload.data);
     case JOIN_GAME:
-        return {playerName: action.meta.userName, sessionId: action.meta.sessionId};
+        return newState(action.meta.userName, action.meta.sessionId);
     default:
       return state
   }
