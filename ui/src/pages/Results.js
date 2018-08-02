489d94d3812f32d4c8a9f71875b41af3177cf177
Richard Ashworth <richardashworth575@gmail.com>
2018-08-02 15:13:38 +0100

    added button

--- ui/src/pages/Results.js
+++ ui/src/pages/Results.js
@@ -4,10 +4,15 @@ import { connect } from 'react-redux';
 class Results extends Component {
 
   render() {
+
+    const adminButton = <button type="button" className="btn btn-primary btn-lg">
+      Next Item
+    </button>
+
     return (
         <div>
         <h2> Results go here! </h2>
-        { this.props.isAdmin ? 'Next item' : 'not admin'}
+        { this.props.isAdmin ? adminButton : '' }
       </div>
     );
   }
