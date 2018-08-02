c87f98855155029f8d4f259815fe8bc8e89fe4bb
Richard Ashworth <richardashworth575@gmail.com>
2018-08-02 14:10:19 +0100

    comment

--- ui/src/reducers/index.js
+++ ui/src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import GameReducer from './reducer_game';
 
-//define what pieces of state each reducer manages
+//keys define what pieces of state each reducer manages
 const rootReducer = combineReducers({
   game: GameReducer
 });
