f67f2be0949ded63acff0f24ae8818baafd4afe2
Richard Ashworth <richardashworth575@gmail.com>
2018-08-05 20:30:47 +0100

    formatting

--- ui/src/reducers/reducer_vote.js
+++ ui/src/reducers/reducer_vote.js
@@ -3,7 +3,8 @@ import {RESULTS_UPDATED, VOTE} from '../actions'
 export default function (state = false, action) {
   switch (action.type) {
     case RESULTS_UPDATED:
-      return (action.payload.length < 1) ? false : action.payload.some(x=>x['userName'] === action.meta.playerName);
+      return (action.payload.length < 1) ? false :
+        action.payload.some(res => res['userName'] === action.meta.playerName);
     case VOTE:
       return true
     default:
