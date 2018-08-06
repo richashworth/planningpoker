26fae377f19f8fcc045e0c225e7971c3538d567d
Richard Ashworth <richardashworth575@gmail.com>
2018-08-06 14:45:57 +0100

    add inf symbol to Constants

--- ui/src/config/Constants.js
+++ ui/src/config/Constants.js
@@ -2,4 +2,6 @@ export const ROOT_URL = 'http://localhost:9000';
 
 export const COFFEE_SYMBOL = '\u2615';
 
+// const INFINITY_SYMBOL= '\u221e';
+
 export const LEGAL_ESTIMATES = [1, 2, 3, 5, 8, 13, 20, 100].map(x => x.toString());
