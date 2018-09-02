25785e87b686604a4933e2152484300bef681a0b
Richard Ashworth <richardashworth575@gmail.com>
2018-09-02 16:11:22 +0100

    update server port

--- planningpoker-web/src/config/Constants.js
+++ planningpoker-web/src/config/Constants.js
@@ -1,4 +1,4 @@
-export const API_ROOT_URL = 'http://planningpoker-tool.herokuapp.com';
+export const API_ROOT_URL = 'https://planningpoker-tool.herokuapp.com';
 
 export const COFFEE_SYMBOL = '\u2615';
 
