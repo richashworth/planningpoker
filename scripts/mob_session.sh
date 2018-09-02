61913e3490ebcbb8e95d4f4dab874ceb206b9360
Richard Ashworth <richardashworth575@gmail.com>
2018-09-02 22:31:56 +0100

    added screenshots

--- scripts/mob_session.sh
+++ scripts/mob_session.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
-for name in "Tom" "Ursula" "Anuj" "Sam" "Isaac" "David" "Gavin"
+for name in "Ben" "Tom" "Ursula" "Anuj" "Sam" "Isaac" "David" "Gavin" "Kirsty"
 do
     echo "${name} is joining session"
-    curl -X POST -F "userName=${name}" -F "sessionId=1" https://planningpoker-tool.herokuapp.com/joinSession;
+    curl -X POST -F "userName=${name}" -F "sessionId=2" https://planningpoker-tool.herokuapp.com/joinSession;
     sleep $((RANDOM % 1))
 done
 
 sleep 2
 
-curl -X POST -F "userName=Tom" -F "sessionId=1" -F "estimateValue=2" https://planningpoker-tool.herokuapp.com/vote;
+curl -X POST -F "userName=Tom" -F "sessionId=2" -F "estimateValue=2" https://planningpoker-tool.herokuapp.com/vote;
 sleep $((RANDOM % 1))
 
-curl -X POST -F "userName=Isaac" -F "sessionId=1" -F "estimateValue=3" https://planningpoker-tool.herokuapp.com/vote;
+curl -X POST -F "userName=Isaac" -F "sessionId=2" -F "estimateValue=3" https://planningpoker-tool.herokuapp.com/vote;
 sleep $((RANDOM % 1))
 
-curl -X POST -F "userName=David" -F "sessionId=1" -F "estimateValue=2" https://planningpoker-tool.herokuapp.com/vote;
+curl -X POST -F "userName=David" -F "sessionId=2" -F "estimateValue=3" https://planningpoker-tool.herokuapp.com/vote;
 sleep $((RANDOM % 1))
 
-curl -X POST -F "userName=Anuj" -F "sessionId=1" -F "estimateValue=20" https://planningpoker-tool.herokuapp.com/vote;
+curl -X POST -F "userName=Anuj" -F "sessionId=2" -F "estimateValue=20" https://planningpoker-tool.herokuapp.com/vote;
 sleep $((RANDOM % 1))
 
-curl -X POST -F "userName=Ursula" -F "sessionId=1" -F "estimateValue=3" https://planningpoker-tool.herokuapp.com/vote;
+curl -X POST -F "userName=Ursula" -F "sessionId=2" -F "estimateValue=3" https://planningpoker-tool.herokuapp.com/vote;
+
+curl -X POST -F "userName=Ben" -F "sessionId=2" -F "estimateValue=5" https://planningpoker-tool.herokuapp.com/vote;
 sleep $((RANDOM % 1))
 
-curl -X POST -F "userName=Gavin" -F "sessionId=1" -F "estimateValue=2" https://planningpoker-tool.herokuapp.com/vote;
+curl -X POST -F "userName=Kirsty" -F "sessionId=2" -F "estimateValue=3" https://planningpoker-tool.herokuapp.com/vote;
+
+curl -X POST -F "userName=Gavin" -F "sessionId=2" -F "estimateValue=2" https://planningpoker-tool.herokuapp.com/vote;
 sleep $((RANDOM % 1))
 
-curl -X POST -F "userName=Sam" -F "sessionId=1" -F "estimateValue=3" https://planningpoker-tool.herokuapp.com/vote;
+curl -X POST -F "userName=Sam" -F "sessionId=2" -F "estimateValue=3" https://planningpoker-tool.herokuapp.com/vote;
 sleep $((RANDOM % 1))
