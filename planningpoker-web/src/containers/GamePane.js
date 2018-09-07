6bdcfd97c24380dfafa59361327cff153396d0dd
Richard Ashworth <richardashworth575@gmail.com>
2018-09-07 21:45:23 +0100

    Added loading screen

--- planningpoker-web/src/containers/GamePane.js
+++ planningpoker-web/src/containers/GamePane.js
@@ -1,20 +1,25 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Loading from '../components/Loading';
 import Vote from '../containers/Vote';
 import Results from '../containers/Results';
 
 class GamePane extends Component {
 
   render() {
-    return (
-      this.props.voted ? <Results/> : <Vote/>
-    );
+    return _isUserVoteRecorded(this.props.userName, this.props.results) ? <Results/> :
+      this.props.voted ? <Loading/> : <Vote/>
   }
+}
+
+function _isUserVoteRecorded(userName, results) {
+  return results.map(x => x['userName']).includes(userName)
 }
 
 function mapStateToProps(state) {
   return {
     voted: state.voted,
+    results: state.results,
   };
 }
 
