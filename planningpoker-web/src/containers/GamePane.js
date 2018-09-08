b3b50a8a612c2cf7395affb3226519091706d709
Richard Ashworth <richardashworth575@gmail.com>
2018-09-08 20:12:53 +0100

    Revert "Revert "Revert "Added loading screen"""
    This reverts commit 4dfcccf

--- planningpoker-web/src/containers/GamePane.js
+++ planningpoker-web/src/containers/GamePane.js
@@ -1,19 +1,15 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import Loading from '../components/Loading';
 import Vote from '../containers/Vote';
 import Results from '../containers/Results';
 
 class GamePane extends Component {
 
   render() {
-    return _isUserVoteRecorded(this.props.userName, this.props.results) ? <Results/> :
-      this.props.voted ? <Loading/> : <Vote/>
+    return (
+      this.props.voted ? <Results/> : <Vote/>
+    );
   }
-}
-
-function _isUserVoteRecorded(userName, results) {
-  return results.map(x => x['userName']).includes(userName)
 }
 
 function mapStateToProps(state) {
