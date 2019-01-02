d5ab2b66089be129d86c9c5795015813a4a00b2d
Richard Ashworth <richardashworth575@gmail.com>
2019-01-02 17:18:55 +0000

    Added Gatling load test

---
+++ planningpoker-api/src/gatling/simulations/com/richashworth/planningpoker/scenarios/CreateGameSimulation.scala
@@ -0,0 +1,26 @@
+package com.richashworth.planningpoker.scenarios
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+
+class CreateGameSimulation extends Simulation {
+
+  private val feeder = csv("users.txt").random.circular
+
+  private val httpConf = http.baseUrl("http://localhost:9000")
+
+  val createGameScenario = scenario("Create Game")
+    .feed(feeder)
+    .exec(
+      http("create game")
+        .post("/createSession")
+        .queryParam("userName", "${userName}")
+        .check(status.is(200))
+    )
+
+  setUp(createGameScenario.inject(
+    rampUsersPerSec(1) to 1000 during (5 seconds)
+  ).protocols(httpConf))
+}
