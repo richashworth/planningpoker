2212793353262c5e2ddb54be077cb05560ed981c
Richard Ashworth <richardashworth575@gmail.com>
2019-09-15 14:03:34 +0100

    Added estimate values

--- planningpoker-web/src/config/Constants.js
+++ planningpoker-web/src/config/Constants.js
@@ -1,7 +1,8 @@
 export const API_ROOT_URL = '';
+// export const API_ROOT_URL = 'http://localhost:9000';
 
 export const COFFEE_SYMBOL = '\u2615';
 
-// const INFINITY_SYMBOL= '\u221e';
+const INFINITY_SYMBOL= '\u221e';
 
-export const LEGAL_ESTIMATES = [1, 2, 3, 5, 8, 13, 20, 100].map(x => x.toString());
+export const LEGAL_ESTIMATES = [0, 0.5, 1, 2, 3, 5, 8, 13, 20, 50, 100, INFINITY_SYMBOL, '?'].map(x => x.toString());
