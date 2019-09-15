da0e639d057e2cada5e382084a18afadafcba2cd
Richard Ashworth <richardashworth575@gmail.com>
2019-09-15 22:38:21 +0100

    Formatting

--- planningpoker-web/src/config/Constants.js
+++ planningpoker-web/src/config/Constants.js
@@ -3,6 +3,6 @@ export const API_ROOT_URL = '';
 
 export const COFFEE_SYMBOL = '\u2615';
 
-const INFINITY_SYMBOL= '\u221e';
+const INFINITY_SYMBOL = '\u221e';
 
 export const LEGAL_ESTIMATES = [0, 0.5, 1, 2, 3, 5, 8, 13, 20, 50, 100, INFINITY_SYMBOL, '?'].map(x => x.toString());
